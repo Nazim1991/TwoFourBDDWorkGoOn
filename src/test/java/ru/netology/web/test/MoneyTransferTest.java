db3a6bf516e4a08668abdb67a09b5d0676207201
Nazim1991 <nazim.said@yandex.ru>
2025-12-07 09:18:36 +0300

    Update MoneyTransferTest.java

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,31 +1,26 @@
 package ru.netology.web.test;
 
-import
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.$;
-import static ru.netology.web.data.DataHelper.getAuthInfo;
 
 class MoneyTransferTest {
     @Test
     void shouldTransferMoneyBetweenOwnCard() {
-        var info = getAuthInfo();
+        var info = DataHelper.getAuthInfo(); // Исправлен вызов метода
         var verificationCode = DataHelper.getVerificationCode(info);
         Selenide.open("http://localhost:9999");
 
         $("[data-test-id='login'] input").setValue(info.getLogin());
         $("[data-test-id='password'] input").setValue(info.getPassword());
-        $("[data-test-id='action-login'] input").click();
+        $("[data-test-id='action-login']").click(); // Убрал .input, если это кнопка
 
-        $("[data-test-id='code'] input").setValue(info.getLogin());
-        $("[data-test-id='login'] input").setValue(verificationCode.getCode());
-        $("[data-test-id=actuon-veryfy]").click();
-
-        $("[data-test-id=dashboard]").should(Condition.visible);
+        $("[data-test-id='code'] input").setValue(verificationCode.getCode()); // Исправлено: вводим код, а не логин
+        // Строка с повторным вводом логина удалена!
+        $("[data-test-id='action-verify']").click(); // Исправлена опечатка
+        $("[data-test-id='dashboard']").should(Condition.visible); // Добавлены кавычки в селектор
     }
-
-
 }
